54915e2e7633e0708041245694d95627b3910ab5
anacc22 <a3jd2cwln@gmail.com>
2024-12-15 14:14:04 +0800

    Fix typo

--- 2024/day_15/part_01.cpp
+++ 2024/day_15/part_01.cpp
@@ -88,7 +88,7 @@ int main() {
   ll ans = 0;
 
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
+    for (int j = 0; j < m; j++) {
       if (g[i][j] == 'O') {
         ans += 100*i + j;
       }
